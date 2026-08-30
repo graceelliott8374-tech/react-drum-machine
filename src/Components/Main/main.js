a85d9223bc80ea50c7d19034d358b2af19e30e38
graceelliott8374-tech <248857877+graceelliott8374-tech@users.noreply.github.com>
2026-08-30 08:54:29 -0400

    Save original Create React App version

---
+++ src/Components/Main/main.js
@@ -0,0 +1,28 @@
+import Bld_H1 from "../Audio/Bld_H1.mp3";
+import Kick_n_Hat from "../Audio/Kick_n_Hat.mp3";
+import Dry_Ohh from "../Audio/Dry_Ohh.mp3";
+import Heater_6 from "../Audio/Heater_6.mp3";
+import Cev_H2 from "../Audio/Cev_H2.mp3";
+import side_stick_1 from "../Audio/side_stick_1.mp3";
+import Brk_Snr from "../Audio/Brk_Snr.mp3";
+import punchy_kick_1 from "../Audio/punchy_kick_1.mp3";
+import Dsc_Oh from "../Audio/Dsc_Oh.mp3";
+
+export const audioData = [
+    {ind: "Bld_H1", id: "Q", name: "cymbal", url: Bld_H1},
+    {ind: "Kick_n_Hat", id: "W", name: "kick", url: Kick_n_Hat},
+    {ind: "Dry_Ohh", id: "E", name: "crash", url: Dry_Ohh}];
+  
+export const audioData2 = [
+    {ind: "Heater_6", id: "A", name: "bass", url: Heater_6},
+    {ind: "Cev_H2", id: "S", name: "scratch", url: Cev_H2},
+    {ind: "side_stick_1", id: "D", name: "sidestick", url: side_stick_1}];
+  
+export const audioData3 = [
+    {ind: "Brk_Snr", id: "Z", name: "snare", url: Brk_Snr},
+    {ind: "punchy_kick_1", id: "X", name: "punchy", url: punchy_kick_1},
+    {ind: "Dsc_Oh", id: "C", name: "drop", url: Dsc_Oh}];
+
+
+
+
